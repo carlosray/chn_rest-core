ea2cfa676c094b43c233a9ff42a96779a6f27e1a
carlosray <carlos.ray@mail.ru>
2021-02-01 17:58:25 +0300

    override flyway initializer

--- src/main/java/ru/vas/restcore/RestCoreApplication.java
+++ src/main/java/ru/vas/restcore/RestCoreApplication.java
@@ -1,14 +1,38 @@
 package ru.vas.restcore;
 
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@RequiredArgsConstructor
 public class RestCoreApplication {
+    private final Flyway flyway;
 
     public static void main(String[] args) {
         SpringApplication.run(RestCoreApplication.class, args);
+    }
+
+    /**
+     * Переопределить flyway initializer по умолчанию чтобы ничего не делать
+     */
+    @Bean
+    FlywayMigrationInitializer flywayInitializer(Flyway flyway) {
+        return new FlywayMigrationInitializer(flyway, (f) -> {
+        });
+    }
+
+    /**
+     * Запустить миграцию в самом конце после генерации DDL
+     */
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> flyway.migrate();
     }
 
 }
